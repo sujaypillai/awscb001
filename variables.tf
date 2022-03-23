cb538334db464f926ef45f87d50ec3ed134a73cb
Sujay Pillai <sujayopillai@gmail.com>
2022-03-23 10:05:47 +0800

    Added changes for CloudTrail configuration

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
 variable "account_ids" {
   description = "List of account IDs allowed to utilise key."
-  default     = []
+  default     = ["435811830822"]
+}
+
+variable "trailName" {
+  description = "AWS CloudTrail Name"
+  default     = "awscbtrail"
+}
+
+variable "trailBucket" {
+  description = "AWS CloudTrail Bucket to store log data"
+  default     = "awscbcloudtrail"
 }
